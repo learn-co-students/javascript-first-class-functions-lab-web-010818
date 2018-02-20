e403e79470fbdf2e0d858c0a53b6d54ace39061c
feihafferkamp <fei.hafferkamp@gmail.com>
2018-02-20 18:57:44 -0500

    Done.

---
+++ index.js
@@ -0,0 +1,19 @@
+function countdown (cb) {
+  window.setTimeout(cb, 2000)
+}
+
+function createMultiplier(num) {
+  return function (multiplierValue) {
+    return multiplierValue * num;
+  }
+}
+
+var doubler = createMultiplier(2);
+var tripler = createMultiplier(3);
+
+function multiplier(arg1, arg2) {
+  return arg1 * arg2;
+}
+
+var doublerWithBind = multiplier.bind(null, 2)
+var triplerWithBind = multiplier.bind(null, 3)
